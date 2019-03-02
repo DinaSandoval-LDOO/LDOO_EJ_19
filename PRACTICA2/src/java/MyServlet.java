14760b3eb6a88c9be19634eede83812cf0f93003
DinaSandoval-LDOO <47577481+DinaSandoval-LDOO@users.noreply.github.com>
2019-03-02 13:49:06 -0600

    Add files via upload

---
+++ PRACTICA2/src/java/MyServlet.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Armando
+ * 
+ 
+ */
+public class MyServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processGetRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String Name = request.getParameter("Nombre");
+            String Email = request.getParameter("Correo");
+            String Password = request.getParameter("Contrasena");
+            String Birthday = request.getParameter("Fecha");
+            
+            out.println("Tu nombre es: " + Name);
+            out.println("Tu correo es: " + Email);
+            out.println("Tu contraseña es : " + Password);
+            out.println("Tu feha de nacimiento es: " + Birthday);
+            
+    }
+}
+    
+    protected void processPostRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String Name = request.getParameter("Nombre");
+            String Email = request.getParameter("Correo");
+            String Password = request.getParameter("Contrasena");
+            String Birthday = request.getParameter("Fecha");
+            
+            out.println("Tu nombre es: " + Name);
+            out.println("Tu correo es: " + Email);
+            out.println("Tu contraseña es : " + Password);
+            out.println("Tu feha de nacimiento es: " + Birthday);
+    }
+}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processGetRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processGetRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
